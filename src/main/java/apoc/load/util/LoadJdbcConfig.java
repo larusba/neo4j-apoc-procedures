30f2b67a36c086bcafc616a19df4bf76c6bbc311
Angelo Busato <angelo.busato@larus-ba.it>
2018-11-19 17:12:23 +0100

    fixes #994 - Provide a configuration parameter map with user/pass for JDBC

--- src/main/java/apoc/load/util/LoadJdbcConfig.java
+++ src/main/java/apoc/load/util/LoadJdbcConfig.java
@@ -1,5 +1,7 @@
 package apoc.load.util;
 
+import org.apache.commons.lang.StringUtils;
+
 import java.time.DateTimeException;
 import java.time.ZoneId;
 import java.util.Collections;
@@ -13,6 +15,8 @@ public class LoadJdbcConfig {
 
     private ZoneId zoneId = null;
 
+    private Map<String,String> credentials;
+
     public LoadJdbcConfig(Map<String,Object> config) {
         config = config != null ? config : Collections.emptyMap();
         try {
@@ -21,10 +25,23 @@ public class LoadJdbcConfig {
         } catch (DateTimeException e) {
             throw new IllegalArgumentException(String.format("The timezone field contains an error: %s", e.getMessage()));
         }
+        this.credentials = config.containsKey("credentials") ? validateCredentials((Map<String, String>) config.get("credentials")) : Collections.emptyMap();
     }
 
     public ZoneId getZoneId(){
         return this.zoneId;
+    }
+
+    public Map<String,String> getCredentials() {
+        return this.credentials;
+    }
+
+    private Map<String, String> validateCredentials(Map<String,String> credentials) {
+        if (!credentials.getOrDefault("user", StringUtils.EMPTY).equals(StringUtils.EMPTY) && !credentials.getOrDefault("password", StringUtils.EMPTY).equals(StringUtils.EMPTY)) {
+            return credentials;
+        } else {
+            throw new IllegalArgumentException("In config param credentials must be passed both user and password.");
+        }
     }
 
 }
